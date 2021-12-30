261e8ed75e796fa0a7f6b8d9a992eaff04ad9b2f
Thomas Broadley <buriedunderbooks@hotmail.com>
2021-12-30 10:11:34 -0500

    Add basic game loop

--- index.ts
+++ index.ts
@@ -1,33 +1,58 @@
-import { random } from "lodash"
+import { random, result } from "lodash";
 
-interface Board {
-
-}
+interface Board {}
 
 interface State {
-    board1: Board;
-    board2: Board;
+  board1: Board;
+  board2: Board;
 }
 
 enum SimulationResult {
-    BOARD_1_WIN = 'BOARD_1_WIN',
-    BOARD_2_WIN = 'BOARD_2_WIN',
-    TIE = 'TIE',
+  BOARD_1_WIN = "BOARD_1_WIN",
+  BOARD_2_WIN = "BOARD_2_WIN",
+  TIE = "TIE",
 }
 
 interface Randoms {
-    nextFrontRowAttack: () => number
+  firstAttack: () => number;
+  nextFrontRowAttack: () => number;
 }
 
-export function simulate(state: State, randoms: Randoms): SimulationResult {
-    console.log(randoms.nextFrontRowAttack())
-    return SimulationResult.TIE
+function result(state: State): SimulationResult | undefined {
+  return undefined;
+}
+
+function simulateAttack(
+  state: State,
+  board1Attacks: boolean,
+  randoms: Randoms
+): State {
+  return state;
+}
+
+export function simulateBrawl(
+  state: State,
+  randoms: Randoms
+): SimulationResult {
+  let currentState = state;
+  let board1Attacks = randoms.firstAttack() === 0;
+
+  while (!result(currentState)) {
+    currentState = simulateAttack(state, board1Attacks, randoms);
+    board1Attacks = !board1Attacks;
+  }
+
+  return result(currentState)!;
 }
 
 const realRandoms: Randoms = {
-    nextFrontRowAttack() {
-        return random(4);
-    }
-}
+  firstAttack() {
+    return random(2);
+  },
 
-console.log(simulate({ board1: {}, board2: {} }, realRandoms))
+  nextFrontRowAttack() {
+    return random(4);
+  },
+};
+
+console.log(simulateBrawl({ board1: {}, board2: {} }, realRandoms));
