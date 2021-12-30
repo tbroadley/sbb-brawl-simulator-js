e68a91705abbacf63b069658f8211d6e956a7c44
Thomas Broadley <buriedunderbooks@hotmail.com>
2021-12-30 09:43:46 -0500

    Initialize project

---
+++ index.js
@@ -0,0 +1,6 @@
+"use strict";
+exports.__esModule = true;
+var lodash_1 = require("lodash");
+(0, lodash_1.times)(3, function () {
+    console.log("Hello world");
+});
---
+++ index.ts
@@ -0,0 +1,5 @@
+import { times } from 'lodash';
+
+times(3, () => {
+    console.log("Hello world");
+});
